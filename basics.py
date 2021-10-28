b28a18ee2018ecb2b3d93e1dbcbdfb4549feead3
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-10-28 11:14:55 -0400

    added input and if/else to evaulate even odd of number

--- basics.py
+++ basics.py
@@ -7,3 +7,9 @@ list_loop = []
 for i in range(10):
     list_loop.append(i)
 print(list_loop)
+
+input1 = int(input("a number "))
+if input1 % 2 == 0:
+    print("the number you entered is even")
+else:
+    print("the number you entered is odd")
