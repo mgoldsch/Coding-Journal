3652fdf348ec4c56ecaa9ed37d4b408a2411a5f3
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-11-13 15:46:14 -0500

    created kid class

---
+++ app/models/kid.py
@@ -0,0 +1,7 @@
+from app.models.family_member import FamilyMember
+from app.models.person import Person
+
+
+class Kid(Person, FamilyMember):
+    def __init__(self, name):
+        super(Kid, self).__init__(name=name)
