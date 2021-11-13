1359a8f6d8c08755ee9075b02f596e3aeb96efbb
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-11-13 16:39:57 -0500

    added trigger greeting method to family member class

--- app/models/family_member.py
+++ app/models/family_member.py
@@ -2,3 +2,7 @@ class FamilyMember:
     def __init__(self, greeting):
         self.family_name = ""
         self.greeting = greeting
+
+    def trigger_greeting(self):
+        print(self.greeting)
+        
