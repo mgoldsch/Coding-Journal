950d34538afa302949c511f7d695c3c8dcc1e6b8
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-11-13 15:44:41 -0500

    created parent class

---
+++ app/models/parent.py
@@ -0,0 +1,7 @@
+from app.models.family_member import FamilyMember
+from app.models.person import Person
+
+
+class Parent(Person, FamilyMember):
+    def __init__(self, name):
+        super(Parent, self).__init__(name=name)
--- app/models/person.py
+++ app/models/person.py
@@ -1,4 +1,3 @@
 class Person:
     def __init__(self, name):
         self.name = name
-        
