4f720e4d9b908bde5da44a384e3e91c8ab45a7a2
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-11-13 15:55:40 -0500

    added test code in main.py of creating a family

---
+++ main.py
@@ -0,0 +1,15 @@
+from app.models.dog import Dog
+from app.models.family import Family
+from app.models.kid import Kid
+from app.models.parent import Parent
+
+kid1 = Kid("Joe")
+parent1 = Parent("Mom")
+parent2 = Parent("Dad")
+dog = Dog("Dog")
+
+family_members = [kid1, parent1, parent2, dog]
+family_name = "LastName"
+
+test_family = Family(family_name, family_members)
+print(test_family.get_members([Dog]))
