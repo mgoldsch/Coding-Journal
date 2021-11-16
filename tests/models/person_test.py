11ce64bb0ec8b87395760e5ee1ac7f69d4609eec
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-11-16 13:16:48 -0500

    added test methods to person_test.py that will require changes to object classes to pass

---
+++ tests/models/person_test.py
@@ -0,0 +1,14 @@
+import pytest
+
+from app.models.person import Person
+
+
+class TestPerson:
+
+    def test_full_name(self):
+        test_person = Person(first_name = 'Joe', last_name = 'Smith')
+        assert test_person.full_name() == 'Joe Smith'
+
+    def test_preferred_name(self):
+        test_person = Person(first_name='Joe', last_name='Smith', preferred_name = 'Joey')
+        assert test_person.full_name() == 'Joey Smith'
