24333f37573ebf257db09dbe8a4d333ddd5c1332
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-11-16 13:30:02 -0500

    made last_name and preferred_name optional for Person class

--- app/models/person.py
+++ app/models/person.py
@@ -1,12 +1,11 @@
 class Person:
-    def __init__(self, first_name, last_name, preferred_name, **kwargs):
+    def __init__(self, first_name, last_name=None, preferred_name=None, **kwargs):
         self.first_name = first_name
         self.last_name = last_name
         self.preferred_name = preferred_name
 
     def full_name(self):
-        if self.preferred_name != "":
-            return self.preferred_name + " " + self.last_name
-        else:
+        if self.preferred_name is None:
             return self.first_name + " " + self.last_name
-        
+        else:
+            return self.preferred_name + " " + self.last_name
