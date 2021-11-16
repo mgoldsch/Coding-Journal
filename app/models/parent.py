7283fb05ec5c547faa15c5822a50a5f8fd301367
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-11-16 13:32:49 -0500

    added other name variables to parent class

--- app/models/parent.py
+++ app/models/parent.py
@@ -3,5 +3,6 @@ from app.models.person import Person
 
 
 class Parent(Person, FamilyMember):
-    def __init__(self, name):
-        super(Parent, self).__init__(first_name=name, greeting="Hello")
+    def __init__(self, first_name, last_name=None, preferred_name=None):
+        super(Parent, self).__init__(first_name=first_name, last_name=last_name,
+                                     preferred_name=preferred_name, greeting="Hello")
