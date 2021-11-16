62236c547d8e70bf3ad604631094b001c8d35a7c
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-11-16 13:33:33 -0500

    added other name variables to kid class

--- app/models/kid.py
+++ app/models/kid.py
@@ -3,5 +3,6 @@ from app.models.person import Person
 
 
 class Kid(Person, FamilyMember):
-    def __init__(self, name):
-        super(Kid, self).__init__(first_name=name, greeting="Hey!")
+    def __init__(self, first_name, last_name=None, preferred_name=None):
+        super(Kid, self).__init__(first_name=first_name, last_name=last_name,
+                                  preferred_name=preferred_name, greeting="Hey!")
