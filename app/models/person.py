7af66812176e375187dab56ac2b36a0c273eec7b
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-11-16 13:25:51 -0500

    added last_name and preferred_name to Person class

--- app/models/person.py
+++ app/models/person.py
@@ -1,3 +1,5 @@
 class Person:
-    def __init__(self, first_name, **kwargs):
+    def __init__(self, first_name, last_name, preferred_name, **kwargs):
         self.first_name = first_name
+        self.last_name = last_name
+        self.preferred_name = preferred_name
