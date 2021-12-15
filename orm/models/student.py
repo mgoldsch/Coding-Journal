7db237ee48ad60a7b610d43949a32adf6f9c76e9
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-12-15 09:59:47 -0500

    created relationship for college and student

--- orm/models/student.py
+++ orm/models/student.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, DateTime, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship, backref
 from models import base
 
+from models.college import College
 
 class Student(base.get_base()):
     __tablename__ = 'students'
@@ -9,6 +10,9 @@ class Student(base.get_base()):
     first_name = Column(String)
     last_name = Column(String)
     class_year = Column(Integer)
+
+    college_id = Column(Integer, ForeignKey('colleges.id'))
+    college = relationship(College, backref="students")
 
     def __init__(self, first_name, last_name, class_year):
         self.first_name = first_name
