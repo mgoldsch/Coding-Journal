350667b9d0886ba52920b8877e36d8bd6b45dad9
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-12-15 09:26:56 -0500

    moved base to separte module

--- orm/models/student.py
+++ orm/models/student.py
@@ -1,8 +1,7 @@
 from sqlalchemy import Column, DateTime, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy.ext.declarative import declarative_base
+from orm.models import base
 
-Base = declarative_base()
 
-class Student(Base):
+class Student(base.get_base()):
     pass
