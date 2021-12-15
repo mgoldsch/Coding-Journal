a50aedc0460c173f03190c18491e26b49e530612
Michael Goldschlager <michaelgoldschlager@michaels-air.dhcp.nd.edu>
2021-12-15 09:30:00 -0500

    created init method for student class

--- orm/models/student.py
+++ orm/models/student.py
@@ -9,3 +9,8 @@ class Student(base.get_base()):
     first_name = Column(String)
     last_name = Column(String)
     class_year = Column(Integer)
+
+    def __init__(self, first_name, last_name, class_year):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.class_year = class_year
